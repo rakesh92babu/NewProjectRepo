9179429051b82b33b8e60886c028d452425929a6
student <student@Achievers-iMac.local>
2017-09-13 10:35:04 +0530

    Initial Commit

---
+++ NewProject2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject2
+//
+//  Created by student on 9/13/17.
+//  Copyright © 2017 Rakesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProject2
+//
+//  Created by student on 9/13/17.
+//  Copyright © 2017 Rakesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
